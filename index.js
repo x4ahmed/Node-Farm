bc5973994357e66db4282ba7a6bcfba1fda0f43b
x4ahmed <x4ahmed.mostafa@outlook.com>
2022-04-10 17:51:07 +0200

    removed unnecessary comments

--- index.js
+++ index.js
@@ -3,29 +3,6 @@ const http = require('http');
 const { parse } = require('path');
 const url = require('url');
 
-// Blocking, synchronous way
-// const textIn = fs.readFileSync('./starter/txt/input.txt','utf-8');
-// console.log(textIn);
-
-// const textOut = `This is what we know about the Avocado: ${textIn}.\nCreated on: ${Date.now()}`
-// fs.writeFileSync('./starter/txt/output.txt', textOut);
-// console.log('File is written!');
-
-// Non-blocking, asynchronous way
-
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-//             console.log(data3);
-//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//                 console.log('Your file has been written! 😂');
-//             })
-//             });
-//         });
-// });
-// console.log('Will read file!');
-
 //////////////////////////////////////
 // SERVER
 const replaceTemplate = (temp, product) => {
